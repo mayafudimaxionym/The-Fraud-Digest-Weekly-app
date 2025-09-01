3885565b264b03a9e42e8d490eb1ef5325dd7fa7
Maya Fudim <maya.fudim@axionym.com>
2025-09-01 16:01:51 +0300

    fix(infra): synchronize terraform output variable names

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,11 @@
+// terraform/outputs.tf
 
-output "project_id" {
-  value = var.project_id
+output "gcp_project_id" {
+  description = "The project ID where resources are deployed."
+  value       = var.gcp_project_id
 }
 
-output "region" {
-  value = var.region
+output "gcp_region" {
+  description = "The region where resources are deployed."
+  value       = var.gcp_region
 }
