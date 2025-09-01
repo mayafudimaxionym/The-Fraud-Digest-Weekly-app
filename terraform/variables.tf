038b6c32a10f1ebe90138099a8a427612afe440a
Maya Fudim <maya.fudim@axionym.com>
2025-09-01 15:59:11 +0300

    fix(infra): synchronize terraform variable names with CI

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,36 @@
+// terraform/variables.tf
 
-variable "project_id" {
-  description = "The project ID to host the solution"
+variable "gcp_project_id" {
   type        = string
+  description = "The Google Cloud project ID where resources will be deployed."
 }
 
-variable "region" {
-  description = "The region to host the solution"
+variable "gcp_region" {
   type        = string
-  default     = "us-central1"
+  description = "The Google Cloud region for deploying resources."
+  default     = "europe-west1"
+}
+
+variable "frontend_service_name" {
+  type        = string
+  description = "The name of the frontend Cloud Run service."
+  default     = "fraud-digest-frontend"
+}
+
+variable "repository_id" {
+  type        = string
+  description = "The ID of the Artifact Registry repository for Docker images."
+  default     = "fraud-digest-repo"
+}
+
+variable "pubsub_topic_id" {
+  type        = string
+  description = "The ID of the Pub/Sub topic for job submissions."
+  default     = "fraud-digest-jobs"
 }
 
 variable "gcp_service_list" {
-  description = "The list of GCP services to enable"
+  description = "The list of GCP services to enable for the frontend"
   type        = list(string)
   default = [
     "iam.googleapis.com",
@@ -19,6 +38,13 @@ variable "gcp_service_list" {
     "cloudbuild.googleapis.com",
     "artifactregistry.googleapis.com",
     "iamcredentials.googleapis.com",
-    "storage.googleapis.com"
+    "pubsub.googleapis.com"
   ]
 }
+
+variable "gcp_support_email" {
+  type        = string
+  description = "The email address to display on the OAuth consent screen."
+  default     = "maya.fudim@axionym.com"
+}
+
